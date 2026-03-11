ee0e4e4111a1891831a15949a9a19c965613d4a2
vaqsi1990 <vaqsii23@gmail.com>
2026-03-11 16:35:26 +0400

    physic

--- app/physical/invoices/page.tsx
+++ app/physical/invoices/page.tsx
@@ -116,10 +116,8 @@ export default function PhysicalInvoicesPage() {
 
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
-    const day = date.getDate();
     const month = date.getMonth();
-    const year = date.getFullYear();
-    
+  
     const months = [
       "იანვარი",
       "თებერვალი",
@@ -134,8 +132,8 @@ export default function PhysicalInvoicesPage() {
       "ნოემბერი",
       "დეკემბერი",
     ];
-    
-    return `${day} ${months[month]}, ${year}`;
+  
+    return months[month];
   };
 
   const formatDateGe = (date: string | Date) => {
